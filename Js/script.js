c99b615d9d3af72fbef106800b912cbdee804d50
Pavel <penaltyoffice@gmail.com>
2021-09-10 21:01:45 +0400

    firstEstate

--- Js/script.js
+++ Js/script.js
@@ -12,7 +12,7 @@ const a= prompt('Один из последних просмотренных ф�
       c= prompt('Один из последних просмотренных фильмов?', ''),
       d= prompt('На сколько оцените его?', '');
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
